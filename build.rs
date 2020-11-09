c137db1c9e501abed5ca0ef2f9e27b056a6658a3
Kaspar Schleiser <kaspar@schleiser.de>
2020-11-09 13:23:25 +0100

    make use of USEMODULE as passed by riot-sys

--- build.rs
+++ build.rs
@@ -7,7 +7,18 @@ fn main() {
         .expect("DEP_RIOT_SYS_CFLAGS is not set, check whether riot-sys exports it.");
     let cflags = shlex::split(&cflags).expect("Odd shell escaping in CFLAGS");
 
+    // riot-sys can pass used modules in DEP_RIOT_SYS_USEMODULE
+    // if it does, handle those here.
+    // alternatively, parse "-DMODULE_" below.
+    if let Some(modules) = env::var("DEP_RIOT_SYS_USEMODULE").ok() {
+        let modules = shlex::split(&modules).expect("Odd shell escaping in USEMODULE");
+        for module in &modules {
+            println!("cargo:rustc-cfg=riot_module_{}", module);
+        }
+    }
+
     println!("cargo:rerun-if-env-changed=DEP_RIOT_SYS_CFLAGS");
+    println!("cargo:rerun-if-env-changed=DEP_RIOT_SYS_USEMODULE");
 
     for flag in cflags.iter() {
         if flag.starts_with("-DMODULE_") {
